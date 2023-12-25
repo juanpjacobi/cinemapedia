83f93cd427b817001a85b422d0c5d6070ad47e5d
= <=>
2023-12-25 20:39:33 -0300

    Actor structure finished

--- lib/infraestructure/repositories/movie_repository_impl.dart
+++ lib/infraestructure/repositories/movie_repository_impl.dart
@@ -1,6 +1,6 @@
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:cinemapedia/domain/repositories/movies.repository.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
 
 class MovieRespositoryImpl extends MoviesRepository {
   final MoviesDataSource dataSource;
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -17,6 +18,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
   void initState() {
     super.initState();
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+    ref.read(actorsByMovieProvider.notifier).loadActors(widget.movieId);
   }
 
   @override
